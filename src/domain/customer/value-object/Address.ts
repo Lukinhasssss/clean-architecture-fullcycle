13c2c7fb2d3c7d056646d29eee929888da3952f0
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-08-11 22:16:20 -0300

    chore: projeto criado

---
+++ src/domain/customer/value-object/Address.ts
@@ -0,0 +1,44 @@
+export default class Address {
+  _street: string = ''
+  _number: number = 0
+  _city: string = ''
+  _zip: string = ''
+
+  constructor (street: string, number: number, city: string, zip: string) {
+    this._street = street
+    this._number = number
+    this._city = city
+    this._zip = zip
+    this.validate()
+  }
+
+  get street (): string {
+    return this._street
+  }
+
+  get number (): number {
+    return this._number
+  }
+
+  get city (): string {
+    return this._city
+  }
+
+  get zip (): string {
+    return this._zip
+  }
+
+  validate (): void {
+    if (!this._street) throw new Error('Address street is required')
+
+    if (!this._number) throw new Error('Address number is required')
+
+    if (!this._city) throw new Error('Address city is required')
+
+    if (!this._zip) throw new Error('Address zip is required')
+  }
+
+  toString (): string {
+    return `${this._street}, ${this._number}, ${this._city}, ${this._zip}`
+  }
+}
