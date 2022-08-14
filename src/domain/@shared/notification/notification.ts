154a5e36a71402e3998eacf04b6b81d0157b99ea
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-08-14 15:25:25 -0300

    feat: implementado notification pattern

---
+++ src/domain/@shared/notification/notification.ts
@@ -0,0 +1,28 @@
+export interface NotificationError {
+  message: string
+  context: string
+}
+
+export default class Notification {
+  private readonly errors: NotificationError[] = []
+
+  addError (error: NotificationError): void {
+    this.errors.push(error)
+  }
+
+  messages (context?: string): string {
+    let message = ''
+    this.errors.forEach(error => {
+      if (!context || error.context === context) { // if context is not defined or if context is equal to error context
+        message += `${error.context}: ${error.message}, `
+      }
+    })
+
+    message = message.slice(0, -2)
+    return message
+    // return this.errors
+    //   .filter(error => error.context === context)
+    //   .map(error => error.message)
+    //   .join(', ')
+  }
+}
