2149954113724baf64763b021fe16d62ebe2e6f8
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-08-14 11:50:16 -0300

    feat: criado rota /customers para listar os clientes

--- src/infrastructure/api/routes/customers.route.ts
+++ src/infrastructure/api/routes/customers.route.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express'
 import CreateCustomerUseCase from '../../../usecase/customer/create/CreateCustomerUseCase'
+import ListCustomerUseCase from '../../../usecase/customer/list/ListCustomerUseCase'
 import CustomerRepository from '../../customer/repository/sequelize/CustomerRepository'
 
 export const customersRoute = express.Router()
@@ -23,6 +24,17 @@ customersRoute.post('/', async (request: Request, response: Response) => {
     const output = await createCustomerUseCase.execute(customerDto)
 
     response.status(201).send(output)
+  } catch (error) {
+    response.status(500).send(error)
+  }
+})
+
+customersRoute.get('/', async (request: Request, response: Response) => {
+  const listCustomerUseCase = new ListCustomerUseCase(new CustomerRepository())
+
+  try {
+    const output = await listCustomerUseCase.execute()
+    response.status(200).send(output)
   } catch (error) {
     response.status(500).send(error)
   }
