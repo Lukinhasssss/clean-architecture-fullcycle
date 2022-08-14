ac4beebe72950c9df3cd952bccfc88093051040a
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-08-14 11:50:49 -0300

    test: adicionado teste e2e para a rota de listagem de clientes

--- src/infrastructure/api/__tests__/customer.e2e.spec.ts
+++ src/infrastructure/api/__tests__/customer.e2e.spec.ts
@@ -39,4 +39,52 @@ describe('E2E tests for customer', () => {
 
     expect(response.status).toBe(500)
   })
+
+  test('should return a list of customers', async () => {
+    await request(app)
+      .post('/customers')
+      .send({
+        name: 'Monkey D Luffy',
+        address: {
+          street: 'Street',
+          city: 'City',
+          number: 1,
+          zipCode: '09876-090'
+        }
+      })
+
+    await request(app)
+      .post('/customers')
+      .send({
+        name: 'Roronoa Zoro',
+        address: {
+          street: 'Street',
+          city: 'City',
+          number: 1,
+          zipCode: '09876-090'
+        }
+      })
+
+    const response = await request(app)
+      .get('/customers')
+
+    expect(response.status).toBe(200)
+    expect(response.body.customers.length).toBe(2)
+
+    const customer1 = response.body.customers[0]
+    const customer2 = response.body.customers[1]
+
+    expect(customer1.id).toBeDefined()
+    expect(customer1.name).toBe('Monkey D Luffy')
+    expect(customer1.address.street).toBe('Street')
+    expect(customer1.address.city).toBe('City')
+    expect(customer1.address.number).toBe(1)
+    expect(customer1.address.zipCode).toBe('09876-090')
+    expect(customer2.id).toBeDefined()
+    expect(customer2.name).toBe('Roronoa Zoro')
+    expect(customer2.address.street).toBe('Street')
+    expect(customer2.address.city).toBe('City')
+    expect(customer2.address.number).toBe(1)
+    expect(customer2.address.zipCode).toBe('09876-090')
+  })
 })
