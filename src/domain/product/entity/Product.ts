fdd1990add73870fce77a446026927f55ac7e522
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-08-16 19:56:50 -0300

    implementado notification pattern na entidade product

--- src/domain/product/entity/Product.ts
+++ src/domain/product/entity/Product.ts
@@ -1,19 +1,20 @@
+import Entity from '../../@shared/entity/Entity.abstract'
+import NotificationError from '../../@shared/notification/NotificationError'
 import IProduct from './IProduct'
 
-export default class Product implements IProduct {
-  private readonly _id: string
+export default class Product extends Entity implements IProduct {
   private _name: string
   private _price: number
 
   constructor (id: string, name: string, price: number) {
-    this._id = id
+    super()
+    this.id = id
     this._name = name
     this._price = price
     this.validate()
-  }
 
-  get id (): string {
-    return this._id
+    if (this.notification.hasErrors())
+      throw new NotificationError(this.notification.getErrors())
   }
 
   get name (): string {
@@ -25,19 +26,32 @@ export default class Product implements IProduct {
   }
 
   changeName (name: string): void {
+    if (!name) {
+      this.notification.addError({ message: 'Product name is required', context: 'product' })
+      throw new NotificationError(this.notification.getErrors())
+    }
+
     this._name = name
-    this.validate()
   }
 
   changePrice (price: number): void {
+    if (price <= 0) {
+      this.notification.addError({ message: 'Product price must be greater than zero', context: 'product' })
+      throw new NotificationError(this.notification.getErrors())
+    }
+
     this._price = price
-    this.validate()
   }
 
   validate (): boolean {
-    if (!this._id) throw new Error('Product id is required')
-    if (!this._name) throw new Error('Product name is required')
-    if (this._price <= 0) throw new Error('Product price must be greater than zero')
+    if (!this.id)
+      this.notification.addError({ message: 'Product id is required', context: 'product' })
+
+    if (!this._name)
+      this.notification.addError({ message: 'Product name is required', context: 'product' })
+
+    if (this._price <= 0)
+      this.notification.addError({ message: 'Product price must be greater than zero', context: 'product' })
 
     return true
   }
